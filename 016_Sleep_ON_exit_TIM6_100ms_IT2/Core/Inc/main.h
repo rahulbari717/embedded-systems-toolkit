3a3f806c31e62b009b6f76aabc30db872af11401
rahulbari <rahulbari717@gmail.com>
2025-12-13 09:21:11 +0530

    some codes related to HAL layers ...

---
+++ 016_Sleep_ON_exit_TIM6_100ms_IT2/Core/Inc/main.h
@@ -0,0 +1,22 @@
+/*
+ * main.h
+ *
+ *  Created on: Dec 6, 2025
+ *      Author: Rahul B.
+ */
+
+#ifndef INC_MAIN_H_
+#define INC_MAIN_H_
+
+#include "stm32f4xx_hal.h"
+#include <string.h>
+#include <stdio.h>
+
+#define SYS_CLOCK_FREQ_50_MHZ   50
+#define SYS_CLOCK_FREQ_84_MHZ   84
+#define SYS_CLOCK_FREQ_120_MHZ  120
+
+#define TRUE 					(1)
+#define FALSE 					(0)
+
+#endif /* INC_MAIN_H_ */
---
+++ 016_Sleep_ON_exit_TIM6_100ms_IT2/Core/Src/data.c
@@ -0,0 +1,11 @@
+/*
+ * data.c
+ *
+ *  Created on: Dec 8, 2025
+ *      Author: Rahul B.
+ */
+
+#include "main.h"
+
+char some_data [] = " We are testing SLEEPONEXIT Feature \r\n";
+
